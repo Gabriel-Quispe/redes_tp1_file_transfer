e20dacc6c65e5e56694694f26a7b434393a8aaff
BrianGabrielQuispe <quispebriangabriel@gmail.com>
2026-04-13 11:47:38 -0300

    servidor multi-cliente

---
+++ src/lib/server/socket.py
@@ -0,0 +1,16 @@
+import socket
+
+
+class ServerSocket:
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((host, port))
+
+    def receive(self):
+        return self.sock.recvfrom(65535)
+
+    def close(self):
+        self.sock.close()
+
+    def send(self, data, addr):
+        self.sock.sendto(data, addr)
