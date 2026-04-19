dcec9711b1685fb22aaa69828e26145ccd23b3ad
Brian Gabriel Quispe <bquispe@fi.uba.ar>
2026-04-19 18:38:18 -0300

    enlazar comandos con la capa de aplicación

--- src/lib/command/download.py
+++ src/lib/command/download.py
@@ -1,8 +1,23 @@
+import os
+import socket
+
+from app.msj_serializer import MessageSerializer
+from app.request.download import RequestDownload
+from app.stop_and_wait import StopAndWait
 from cli.download import DownloadCLI
 from params.download import DownloadParams
 
 
 class DownloadCommand:
-    def execute(left):
+    def execute(self):
         args = DownloadCLI().args()
         params = DownloadParams(args)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("0.0.0.0", 0))
+
+        dest_path = os.path.join(params.dst, params.name)
+        rdt = StopAndWait(sock, (params.host, params.port))
+        serializer = MessageSerializer()
+
+        RequestDownload(rdt, serializer, params.name, dest_path).ejecutar()
--- src/lib/command/upload.py
+++ src/lib/command/upload.py
@@ -1,8 +1,21 @@
+import socket
+
+from app.msj_serializer import MessageSerializer
+from app.request.upload import RequestUpload
+from app.stop_and_wait import StopAndWait
 from cli.upload import UploadCLI
 from params.upload import UploadParams
 
 
 class UploadCommand:
-    def execute(left):
+    def execute(self):
         args = UploadCLI().args()
         params = UploadParams(args)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("0.0.0.0", 0))
+
+        rdt = StopAndWait(sock, (params.host, params.port))
+        serializer = MessageSerializer()
+
+        RequestUpload(rdt, serializer, params.src).ejecutar()
