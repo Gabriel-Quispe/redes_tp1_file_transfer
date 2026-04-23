2125c5835f551b55fe9241ea88e1f97fedccddf1
Brian Gabriel Quispe <bquispe@fi.uba.ar>
2026-04-23 06:46:44 -0300

    refactor: simplifico sintaxis del ServerSocket

--- src/lib/server/socket.py
+++ src/lib/server/socket.py
@@ -1,20 +1,20 @@
-import socket
+import socket as s
 
 
 class ServerSocket:
-    def __init__(self, host, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((host, port))
+    SIZE_MAX_BUFFER = 65535
+
+    def __init__(self, host: str, port: str) -> None:
+        self.sk = s.socket(s.AF_INET, s.SOCK_DGRAM)
+        self.sk.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+        self.sk.bind((host, port))
 
     def receive(self):
-        return self.sock.recvfrom(65535)
-
-    def close(self):
-        self.sock.close()
-
-    def send(self, data, addr):
-        self.sock.sendto(data, addr)
+        size = self.SIZE_MAX_BUFFER
+        return self.sk.recvfrom(size)
 
     def sendto(self, data, addr):
-        self.sock.sendto(data, addr)
+        self.sk.sendto(data, addr)
+
+    def close(self):
+        self.sk.close()
