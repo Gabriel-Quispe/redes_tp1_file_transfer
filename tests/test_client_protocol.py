7da7b46287e452515088ed237dc65865f63df7ab
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-23 23:22:06 -0300

    add: carpeta de test para protocolo

---
+++ tests/test_client_protocol.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.lib.model.ftp.segment.const import *
+from src.lib.model.ftp.segment.segment import Segment
+
+
+
+
+
+client_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+dest=('10.0.0.2',65535)
+msg=b"TEST de protocolo"
+for i in range(1,65536):
+    segment=Segment(OP_START_UPLOAD,i,10,msg)
+    client_socket.sendto(segment.pack(),dest)
+print("Enviado!")
