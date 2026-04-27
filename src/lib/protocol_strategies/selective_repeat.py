5e41c2444d10e9c9296869f87ba03e41dd8595fa
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-27 12:54:54 -0300

    base selective repeat

--- src/lib/protocol_strategies/selective_repeat.py
+++ src/lib/protocol_strategies/selective_repeat.py
@@ -1,3 +1,13 @@
-from protocol_strategy import *
+from .protocol_strategy import ProtocolStrategy
 class SelectiveRepeat(ProtocolStrategy):
-    pass
+    def __init__(self, address, socket):
+        super().__init__(address, socket)
+        
+    def set_window(self,tam:int):
+        pass
+    
+    def send_data(self,payload):
+        pass
+    
+    def receive_data():
+        pass
