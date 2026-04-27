c6ccbc84a5d88b808fec67c3772991d9f76e3fd8
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-27 12:54:22 -0300

    clase protocol strategy

--- src/lib/protocol_strategies/protocol_strategy.py
+++ src/lib/protocol_strategies/protocol_strategy.py
@@ -1,9 +1,19 @@
+from typing import Optional
+import socket as sckt
+import lib.const as const
+from lib.segment import Segment
 class ProtocolStrategy:
-    def set_window(tam:int):
+    def __init__(self,address,socket):
+        self.receive_tam=4096 #Esto es más que un paq, por seguridad
+        self.next_seq=1
+        self.address=address
+        self.socket:sckt.socket=socket
+        self.wsize=const.SV_MAX_WIN
+    def set_window(self,tam:int)->None:
         pass
-    
-    def send_data(payload):
+    def send_data(self,payload)-> Optional[Segment]:
         pass
-    
-    def receive_data():
+    def do_handshake(self,segment: Segment)-> Optional[Segment]:
+        return self.send_data(segment)
+    def receive_data(self)-> Optional[bytes]:
         pass
