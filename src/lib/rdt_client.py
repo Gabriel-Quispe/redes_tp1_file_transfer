1cb8c6041c99b8ed4ae59d8ee5345df4dc0ed0b4
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-28 01:10:12 -0300

    clase base para upload y download

---
+++ src/lib/rdt_client.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+import lib.const as const
+from lib.logger import *
+from lib.rdt_socket import FRDTSocket
+
+class FRDTClient:
+    def __init__(self, description):
+        self.parser = argparse.ArgumentParser(description=description)
+        self._add_common_arguments()
+        self.args = None
+        self.rdt_sock = None
+
+    def _add_common_arguments(self):
+        self.parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+        self.parser.add_argument("-q", "--quiet", action="store_true", help="decrease output verbosity")
+        self.parser.add_argument("-H", "--host", default="10.0.0.2", help="server IP address")
+        self.parser.add_argument("-p", "--port", type=int, default=const.PORT, help="server port")
+        self.parser.add_argument("-n", "--name", required=True, help="file name")
+        self.parser.add_argument("-r", "--protocol", type=int, choices=[1, 2], default=1, help="protocol (1: SW, 2: SR)")
+
+    def setup(self):
+        self.args = self.parser.parse_args()
+        configure_logger(self.args.verbose, self.args.quiet)
+        # tanto upload como download usan un file reliable data transfer protocol
+        self.rdt_sock = FRDTSocket((self.args.host, self.args.port), self.args.protocol)
+    def run(self):
+        pass
