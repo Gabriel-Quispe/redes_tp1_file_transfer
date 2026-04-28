cad1b213527898f6570a0252d2fe59edf7efbe51
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-28 01:09:13 -0300

    format

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -1,5 +1,13 @@
 import logging
-
+def show_stats(final_time, size):    
+    throughput=0
+    if final_time>0:
+        throughput=(size)/final_time
+    logger.info("Transmisión finalizada con exito.")
+    logger.info(f"Estadísticas")
+    logger.info(f"Tiempo: {final_time:.2f} segundos")
+    logger.info(f"Tamaño: {size / 1024:.2f} KB")
+    logger.info(f"Throughput promedio: {throughput:.2f} bps")
 def configure_logger(verbose=False, quiet=True):
     if quiet:
         level = logging.ERROR
