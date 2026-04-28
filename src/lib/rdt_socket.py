b3e6ba1aadb6f32049681a72050ab4cf47ba9a7d
Alfredo Godoy <agodoy@fi.uba.ar>
2026-04-28 01:13:12 -0300

    connect ahora se implementa puramente en la transport layer

--- src/lib/rdt_socket.py
+++ src/lib/rdt_socket.py
@@ -23,41 +23,45 @@ class FRDTSocket:
             case _:
                 raise ValueError("Protocolo no soportado!")
         logger.debug(f"Iniciando socket para {self.address} con protocolo {self.p_strategy.__class__}")
-    #hanshake
-    def connect(self,op_start, file_name=None, size=0):
-        # lado del cliente
-        if file_name is not None:
-            logger.debug(f"Intenando subir {file_name} de {size} bytes ")
-            data_payload = struct.pack("!BQ", self.protocol_id, size) + file_name.encode()
-            # segment inicial con:
-            # operacion de inicio
-            # seqnumber en 0
-            # tamaño de ventana maxima! (la propone el cliente)
-            # informacion sobre el protocolo y el archivo en el payload
-            request_segment = Segment(op_start,0,self.wsize,data_payload)
-            # el request lo envia la strategy (porque se puede perder)
-            server_response_segment: Segment=self.p_strategy.do_handshake(request_segment)
-            if not server_response_segment:
-                logger.error("El servidor no respondio al handshake")
-                raise ConnectionError("Error de conexion!")
-            self.wsize = server_response_segment.wsize
-            self.p_strategy.set_window(self.wsize)
-            logger.info(f"Conexion establecida!, ventana receptora {self.wsize}")
-            return server_response_segment
-        else:#somos el servidor
-            logger.debug("Servidor esperando paquete inicial...")
-            raw_data, addr = self.socket.recvfrom(const.MAX_PAYLOAD_SIZE)
-            
-            self.address = addr
-            self.p_strategy.address = addr 
-            client_request = Segment.unpack(raw_data)
-            logger.debug(f"Pedido recibido de {addr}. Enviando confirmacion...")
-            self.wsize = const.SV_MAX_WIN // const.SV_MAX_CLIENTS
-            response_segment = Segment(const.OP_ACK, 0, self.wsize, b"")            
-            self.socket.sendto(response_segment.pack(), self.address)
-            
-            self.p_strategy.set_window(self.wsize)            
-            return client_request
+    
+    # este handshake viene del lado del cliente.
+    # la app inicia la conexion enviando un payload con info ya preparado
+    # debe reintentar hasta poder conectarse.
+    def connect(self, op_start, payload=b"") -> Segment:
+        logger.debug(f"Iniciando conexión con opcode {op_start}")
+        # la capa de transporte mete el payload en un segmento
+        request_segment = Segment(op_start, 0, self.wsize, payload)
+        
+        # la estrategia elegida se encarga de asegurar el handshake,
+        # devolviendo el segmento de respuesta del server
+        response_segment = self.p_strategy.do_handshake(request_segment)
+        # luego del handshake, debe retornar la nueva direccion dada por el server
+        self.address=self.p_strategy.address
+        
+        #flujo de errores
+        if not response_segment:
+            logger.error("El servidor no respondió al handshake del cliente")
+            raise ConnectionError("Error: Timeout en handshake")                    
+        # Actualizamos ventana según lo que el servidor nos permitió
+        self.wsize = response_segment.wsize
+        self.p_strategy.set_window(self.wsize)        
+        logger.info(f"Conexión establecida. Ventana del servidor: {self.wsize}")
+        return response_segment
+    
+    
+    # este metodo se utiliza para finalizar el handshake inicial, dado que el cliente usa un rdt, 
+    # intentara conectarse al servidor hasta lograrlo, el servidor sin embargo, no necesita reintentar nada
+    # todavia
+    # accept_connection no lee nada de ningun socket, se asume que el server lo leyo
+    def accept_connection(self, initial_segment):
+        self.wsize = const.SV_MAX_WIN // const.SV_MAX_CLIENTS        
+        #respuesta ack al start
+        response_segment = Segment(const.OP_ACK, 0, self.wsize, b"")        
+        logger.debug(f"Enviando ACK inicial a {self.address} de un nuevo puerto")
+        self.socket.sendto(response_segment.pack(), self.address)
+        self.p_strategy.set_window(self.wsize)        
+        return initial_segment
+    
     
     def send(self,data):
         size = const.MAX_PAYLOAD_SIZE
@@ -65,16 +69,15 @@ class FRDTSocket:
         for i in range(0, len(data), size):
             payload=data[i:i + size]
             segment = Segment(const.OP_DATA, self.next_seq, self.wsize, payload)
-            logger.debug(f"Enviando DATA con seq={self.next_seq}")
+            logger.debug(f"Enviando DATA con seq={self.p_strategy.next_seq}")
             
             # La estrategia se encarga de que este segmento llegue
             self.p_strategy.send_data(segment)
             
             self.next_seq += 1
-    def recv(self):
-        logger.debug(f"Recibiendo DATA")
-        payload = self.p_strategy.receive_data()
-        return payload
+    def recv(self)->Tuple[int, Optional[bytes]]:
+        logger.debug(f"Recibiendo paquete")         
+        return self.p_strategy.receive_data()
     
     #def close(self):
     #    logger.info(f"cerrando conexion")
@@ -83,7 +86,7 @@ class FRDTSocket:
     #    self.socket.close()
     def close(self):
         logger.info(f"cerrando conexion")
-        fin_segment = Segment(const.OP_END, self.p_strategy.next_seq, 0, b"")
+        fin_segment = Segment(const.OP_END, self.p_strategy.next_seq, 1, b"")
         # No necesito que sea confiable el close! (se quedan infinitamente esperando el ack del ack
         # como dijeron en clase con el ejemplo de los generales)
         self.socket.settimeout(const.TIMEOUT)
